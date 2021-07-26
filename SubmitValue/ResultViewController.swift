03502b4251b202b6ec7f4dd671fb056d20f7c507
ujeon_jang <wj.jang@newploy.net>
2021-07-26 12:13:05 +0900

    다른 뷰 컨트롤러에 값을 직접 전달하는 방법

---
+++ SubmitValue/ResultViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet var resultEmail: UILabel!
+    @IBOutlet var resultUpdate: UILabel!
+    @IBOutlet var resultInterval: UILabel!
+    
+    var paramEmail: String = ""
+    var paramUpdate: Bool = false
+    var paramInterval: Double = 0
+    
+    override func viewDidLoad() {
+        self.resultEmail.text = paramEmail
+        self.resultUpdate.text = (self.paramUpdate == true ? "자동 갱신" : "자동 갱신 안함")
+        self.resultInterval.text = "\(Int(paramInterval))분 마다 갱신"
+    }
+    
+    @IBAction func onBack(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
+}
--- SubmitValue/ViewController.swift
+++ SubmitValue/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var email: UITextField!
+    @IBOutlet var isUpdate: UISwitch!
+    @IBOutlet var interval: UIStepper!
+    @IBOutlet var isUpdateText: UILabel!
+    @IBOutlet var intervalText: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true) {
+            self.isUpdateText.text = "갱신함"
+        } else {
+            self.isUpdateText.text = "갱신하지 않음"
+        }
+    }
+    
+    @IBAction func onStepper(_ sender: Any) {
+        var value: Int = 0
+        if let castedSender = sender as? UIStepper {
+            value = Int(castedSender.value)
+        }
+        self.intervalText.text = "\(value)분 마다"
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "RVC") as? ResultViewController else {
+            return
+        }
+        
+        rvc.paramEmail = self.email.text ?? ""
+        rvc.paramUpdate = self.isUpdate.isOn
+        rvc.paramInterval = self.interval.value
+        
+//        self.present(rvc, animated: true)
+        self.navigationController?.pushViewController(rvc, animated: true)
+    }
+    
+    @IBAction func onPerformSegue(_ sender: Any) {
+        self.performSegue(withIdentifier: "ManualSubmit", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dest = segue.destination
+        
+        guard let rvc = dest as? ResultViewController else {
+            return
+        }
+        
+        rvc.paramEmail = self.email.text ?? ""
+        rvc.paramUpdate = self.isUpdate.isOn
+        rvc.paramInterval = self.interval.value
+    }
 }
 
